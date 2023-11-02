48fdcd9d9fdb33200b71130509a16086097be960
aaron <xietianle@outlook.com>
2023-11-02 11:42:00 +1100

    Stop tracking firebaseConfig.js

--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ import "firebase/firestore"; // If you plan to use Firestore
 import "firebase/auth"; // If you plan to use Firebase Authentication
 
 // pls use your own config to replace this
+// Your web app's Firebase configuration
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 } else {
